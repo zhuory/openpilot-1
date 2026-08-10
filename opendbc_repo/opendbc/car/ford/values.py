4a3d6c687a6c949f1656787a4f0011efb6efe3a5
RAINNY4114 <152569703+RAINNY4114@users.noreply.github.com>
2026-08-10 23:28:49 +0800

    Update values.py

--- opendbc_repo/opendbc/car/ford/values.py
+++ opendbc_repo/opendbc/car/ford/values.py
@@ -174,6 +174,10 @@ class CAR(Platforms):
     [FordCarDocs("Ford Ranger 2024", "Adaptive Cruise Control with Lane Centering", setup_video="https://www.youtube.com/watch?v=2oJlXCKYOy0")],
     CarSpecs(mass=2000, wheelbase=3.27, steerRatio=17.0),
   )
+  LINCOLN_NAUTILUS = FordPlatformConfig(
+    [FordCarDocs("Lincoln Nautilus 2018-21", "Adaptive Cruise Control with Lane Centering")],
+    CarSpecs(mass=2050, wheelbase=3.025, steerRatio=19.33),
+  )
 
 
 # FW response contains a combined software and part number
