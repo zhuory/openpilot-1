4564848a4088c257b03dfcb1561e0e409f00458c
RAINNY4114 <152569703+RAINNY4114@users.noreply.github.com>
2026-08-10 23:37:26 +0800

    Update paramsd.py

--- openpilot/selfdrive/locationd/paramsd.py
+++ openpilot/selfdrive/locationd/paramsd.py
@@ -163,7 +163,7 @@ class VehicleParamsLearner:
     liveParameters = msg.liveParameters
     liveParameters.posenetValid = True
     liveParameters.sensorValid = sensors_valid
-    liveParameters.steerRatio = float(x[States.STEER_RATIO].item())
+    liveParameters.steerRatio = 19.33
     liveParameters.stiffnessFactor = float(x[States.STIFFNESS].item())
     liveParameters.roll = float(self.roll)
     liveParameters.angleOffsetAverageDeg = float(self.avg_angle_offset)
